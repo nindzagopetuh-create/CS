380ab05bcc8aebed643934352d8b5e84e0013616
dfdfsdfsd3445325345@outlook.com <dfdfsdfsd3445325345@outlook.com>
2025-10-20 15:28:52 +0300

    Add project files.

---
+++ Home_work/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine();
+
+        // 1) Прямоугольник 5x5
+        Console.WriteLine("1)");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // 2) Прямоугольный треугольник (возрастающий)
+        Console.WriteLine("2)");
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // 3) Прямоугольный треугольник (убывающий)
+        Console.WriteLine("3)");
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // 4) Треугольник со смещением вправо (убывающий)
+        Console.WriteLine("4)");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("  ");
+            }
+            for (int j = 0; j < 5 - i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // 5) Треугольник со смещением вправо (возрастающий)
+        Console.WriteLine("5)");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 4 - i; j++)
+            {
+                Console.Write("  ");
+            }
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // 6) Ромб/песочные часы
+        Console.WriteLine("6)");
+        // Верхняя часть
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 4 - i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("/");
+            for (int j = 0; j < i * 2; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("\\");
+            Console.WriteLine();
+        }
+        // Нижняя часть
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("\\");
+            for (int j = 0; j < (4 - i) * 2; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("/");
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // 7) Шахматная доска 5x5
+        Console.WriteLine("7)");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if ((i + j) % 2 == 0)
+                    Console.Write("+ ");
+                else
+                    Console.Write("- ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Home_work/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Home_work")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Home_work")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d6d9e8b-5e74-4cf0-a4d1-0c91d2bed856")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
